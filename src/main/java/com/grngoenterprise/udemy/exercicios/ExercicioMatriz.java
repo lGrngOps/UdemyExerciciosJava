165767796184f69e5e1ff3228e7b203262342a18
GringOps <railson.sistemas@gmail.com>
2023-12-05 15:45:15 -0300

    Exercicio Matriz

---
+++ src/main/java/com/grngoenterprise/udemy/exercicios/ExercicioMatriz.java
@@ -0,0 +1,32 @@
+package com.grngoenterprise.udemy.exercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ExercicioMatriz {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int qtdAlunos = leitor.nextInt();
+        int qtdNotas = leitor.nextInt();
+
+        double[][] notasTurma = new double[qtdAlunos][qtdNotas];
+        double total = 0;
+
+        for (int x=0; x < notasTurma.length; x++){
+            for (int y=0; y < notasTurma[x].length; y++){
+                notasTurma[x][y] = leitor.nextDouble();
+                total += notasTurma[x][y];
+            }
+        }
+
+        double media = total / (qtdAlunos * qtdNotas);
+
+        for (double[] notasAluno: notasTurma){
+            System.out.println(Arrays.toString(notasAluno));
+        }
+        System.out.println(media);
+    }
+}
